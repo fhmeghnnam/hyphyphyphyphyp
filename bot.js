c10eea5cd0621ae3c7423a32009906d1bc9d186e
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2018-12-29 15:05:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,6 +118,35 @@ client.on("message", (message) => {
 
 
 
+client.on('message', message => {
+    var prefix = "!";
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+        let args = message.content.split(' ').slice(1).join(' ');
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+
+        if (!args[1]) {
+    message.channel.send("**!bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       var bc = new Discord.RichEmbed()
+       .addField( `${message.guild.name}`,  m)
+       .addField('From : ', `${message.author.username}`)
+       .addField(' Message ', args)
+       .setColor('RANDOM')
+       m.send({embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+
+
+
 
 
 
