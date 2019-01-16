f1d28aa1654107e1e41223dde6ef7b15d91a5be1
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-01-16 19:27:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2050,7 +2050,7 @@ if (message.content.startsWith(prefix + 'نقاطي')) {
   fs.writeFile("./json/awsmm.json", JSON.stringify(points), (err) => {
     if (err) console.error(err)
   })
-});*/
+});
 
 /*nickname*/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
