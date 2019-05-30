828f1dcbc3d3e39b887a7c04ba8853cc4d6f4700
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-05-30 20:14:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,51 +1,20 @@
+
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
-const db = require('quick.db');
-const premium = ['470896018603376640']
-const client = new Discord.Client();   
-const bot = new Discord.Client();   
-const giphy = require('giphy-api')();    
-const googl = require('goo.gl');  
-const translate = require('google-translate-api');   
-const fs = require("fs"); 
-const canvas = require("canvas");
-const getYoutubeID = require('get-youtube-id'); 
-const moment = require("moment");  
-const { Client, Util } = require('discord.js');  
-const UserBlocked = new Set(); 
-const jimp = require('jimp');   
-const math = require('math-expression-evaluator'); 
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const google = require('google-it'); 
-const queue = new Map(); 
-const zalgo = require('zalgolize');   
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const ytdl = require('ytdl-core');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const sql = require("sqlite");
-const dateFormat = require('dateformat'); 
-const pretty = require('pretty-ms') 
-const prefix = '!';
-var table = require('table').table
-var ti={}  
-,spee={}
-,attentions={};
-
-client.on('ready', () => {
-  let channel = client.channels.get('545326026062823434');
-  channel.join()
+const client = new Discord.Client();
+let prefix = "!"
+client.on('ready',async () => {
+  client.channels.find(ch => ch.id === "580414237403512844" && ch.type === 'voice').join();
 });
 
-bot.on('ready', () => {
-    bot.user.setStatus('available')
-    bot.user.setPresence({
-        game: {
-            name: 'Legends Gang !!',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
-        }
-    });
+client.on("ready", () => {
+    client.user.setStatus("online");
 });
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
