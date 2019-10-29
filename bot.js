d89fe8d6f730699e1f4e8a96a81c75fe3466f254
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-10-29 14:45:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 let prefix = "!"
 client.on('ready',async () => {
-  client.channels.find(ch => ch.id === "613749249968046123" && ch.type === 'voice').join();
+  client.channels.find(ch => ch.id === "634898391285104645" && ch.type === 'voice').join();
 });
 
 client.on("ready", () => {
 
-client.user.setActivity("Every moment is a fresh beginning. ...");
+client.user.setActivity("if you read this I love you");
 });
 
 
