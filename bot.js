737be4e4ee21d07f976fe3d77eded5e589a07535
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-11-07 22:58:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,6 @@ client.on('ready',async () => {
   client.channels.find(ch => ch.id === "613749249968046123" && ch.type === 'voice').join();
 });
 
-client.on("ready", () => {
-
-client.user.setActivity("Send nudes in DM please ");
-});
-
 
 
 
